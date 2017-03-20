f272c2fa003e86f62809cdca742fee7a637052e0
Hadi Farhat <hadi236a@gmail.com>
2017-03-20 20:39:01 +0000

    board works with all required functions

--- battleship.py
+++ battleship.py
@@ -1,3 +1,14 @@
+import pdb
+
+# imports
+from player import Player
+from board import Board
+
+def play():
+    p = Player()
+
+
+
 SHIP_INFO = [
     ("Aircraft Carrier", 5),
     ("Battleship", 4),
@@ -15,19 +26,45 @@ MISS = '.'
 HIT = '*'
 SUNK = '#'
 
+letter_board = 'abcdefghij'
 
 def clear_screen():
     print("\033c", end="")
 
 
 def print_board_heading():
-    print("   " + " ".join([chr(c) for c in range(ord('A'), ord('A') + BOARD_SIZE)]))
+    print("" + " ".join([chr(c) for c in range(ord('A'), ord('A') + BOARD_SIZE)]))
 
 
-def print_board(board):
+
+def get_ships_locations(player_board_ship_gird):
+    ships_locations = []
+    for ship in player_board_ship_gird:
+        for ship_location in ship:
+            ships_locations.append(ship_location)
+    return ships_locations
+
+
+def print_board(ship_locations):
+    print("", end=' ')
     print_board_heading()
+    for row in range(1, 11):
+        print(row, end=' ')
+        for x in letter_board:
+            if (x, row, 'v') in ship_locations:
+                print(VERTICAL_SHIP, end=' ')
+            elif (x, row, 'h') in ship_locations:
+                print(HORIZONTAL_SHIP, end=' ')
+            else:
+                print(EMPTY, end=' ')
+        print("\n")
 
-    row_num = 1
-    for row in board:
-        print(str(row_num).rjust(2) + " " + (" ".join(row)))
-        row_num += 1
+
+
+
+
+
+
+
+
+
--- board.py
+++ board.py
@@ -1,98 +1,140 @@
-import pdb	
+import pdb  
 
 class Board:
-	# The Board class is responsible for:
-	# - Taking user input
-	# - Validating User input:
-	#		- Slice the input to letter and number
-	#		- Validate letter and number
-	# - Saving the input into an object (probably an array)
-	ships = [
-	    ("Aircraft Carrier", 5),
-	    ("Battleship", 4),
-	    ("Submarine", 3),
-	    ("Cruiser", 3),
-	    ("Patrol Boat", 2)
-	]
+    ships = [
+        ("Aircraft Carrier", 5),
+        ("Battleship", 4),
+        ("Submarine", 3),
+        ("Cruiser", 3),
+        ("Patrol Boat", 2)
+    ]
 
-	ship_grid = []
+    def __init__(self):
+        self.set_up()
 
-	def __init__(self):
-		self.set_up()
+    # clears the screen
+    def clear_screen(self):
+        print("\033c", end="")
 
-	# main loop; runs board program
-	def set_up(self):
-		for ship in range(len(self.ships)):
-			while True:
-				ship_location = input("Where do you want"
-									  "to place {}".format(self.ships[ship]))
-				# if user input is valid
-				if self.validate_user_input(ship_location):
-					# get ship locations
-					ship_locations = self.get_ship_locations(ship_location,
-															int(self.ships[ship][1]))
-					# check if ship locations are already in use
-					if self.validate_ship_locations(ship_locations):						
-						print("ship is not in use")
-						print(self.ship_grid)
-						break
-					else:
-						print("can't place ship here ...")
+    # location of all ships related to this object
+    ship_grid = []
 
-	# validate user input
-	def validate_user_input(self, ship_location):
-		if len(ship_location) <= 3:
-			ship_location_x = ship_location[:1]
-			ship_location_y = int(ship_location[1:])
-			if (ship_location_x in 'abcdefghij' and 
-				ship_location_y in [1,2,3,4,5,6,7,8,9,10]):
-			   return True
-		return False
+    # main loop; runs board program
+    def set_up(self):
+        for ship in range(len(self.ships)):
+            while True:
+                ship_location = input("Where do you want"
+                                      "to place {}".format(self.ships[ship]))
+                # if user input is valid
+                if self.validate_user_input(ship_location):
+                    # get ship locations
+                    ship_locations = self.get_ship_locations(ship_location,
+                                                            int(self.ships[ship][1]))
+                    # check if ship locations are already in use
+                    if self.validate_ship_locations(ship_locations):
+                        self.clear_screen()
+                        self.print_board(self.get_ships_locations(self.ship_grid))
+                        break
+                    else:
+                        print("can't place ship here ...")
+                else:
+                    print("Please enter correct input")
 
-	# returns an array containing ship locations on board
-	def get_ship_locations(self, ship_location, ship_length):
-		ship_locations = []
-		direction = self.get_ship_direction()
-		# check if user wants to place ships h or v
-		if direction == 'h':
-			# convert letter to its ascii value
-			# check if it can fit on board
-			if (ord('j') - ord(ship_location[:1])) + 1 >= ship_length:
-				print("ship length can fit on board")
-				for i in range(0, ship_length):					
-					ship_locations.append(((chr(ord(ship_location[:1]) + i)),
-										   int(ship_location[1:])))
-				return ship_locations
-		elif direction == 'v':
-			# check if it can fit on board
-			if (10 - (int(ship_location[1:]))) + 1 >= ship_length:
-				print("ship length can fit on board")
-				for i in range(0, ship_length):
-					ship_locations.append((ship_location[:1],
-										   int(ship_location[1:]) + i))
-				return ship_locations
-		return None
+    # validate user input
+    def validate_user_input(self, ship_location):
+        if len(ship_location) <= 3:
+            ship_location_x = ship_location[:1]
+            try:
+                ship_location_y = int(ship_location[1:])
+            except ValueError:
+                return False
+            if (ship_location_x in 'abcdefghij' and 
+                ship_location_y in [1,2,3,4,5,6,7,8,9,10]):
+               return True
+        return False
 
-	# check if ship location is already in use
-	def validate_ship_locations(self, ship_locations):
-		if ship_locations:
-			if self.ship_grid:
-				for ship in self.ship_grid:
-					for ship_location in ship_locations:
-						if ship_location in ship:
-							return False
-			self.ship_grid.append(ship_locations)
-			return True
-		return False
+    # returns an array containing ship locations on board
+    def get_ship_locations(self, ship_location, ship_length):
+        ship_locations = []
+        direction = self.get_ship_direction()
+        # check if user wants to place ships h or v
+        if direction == 'h':
+            # convert letter to its ascii value
+            # check if it can fit on board
+            if (ord('j') - ord(ship_location[:1])) + 1 >= ship_length:
+                print("ship length can fit on board")
+                for i in range(0, ship_length):                 
+                    ship_locations.append(((chr(ord(ship_location[:1]) + i)),
+                                           int(ship_location[1:]),
+                                           'h'))
+                return ship_locations
+        elif direction == 'v':
+            # check if it can fit on board
+            if (10 - (int(ship_location[1:]))) + 1 >= ship_length:
+                print("ship length can fit on board")
+                for i in range(0, ship_length):
+                    ship_locations.append((ship_location[:1],
+                                           int(ship_location[1:]) + i,
+                                           'v'))
+                return ship_locations
+        return None
 
-	# check if user wants the ship horizontally or vertically
-	def get_ship_direction(self):
-		while True:
-			ship_direction = input("Do you want to place ship"
-								   "[H]orizontally or "
-								   "[V]erticlaly ?").strip().lower()
-			if ship_direction == 'v':
-				return 'v'
-			elif ship_direction == 'h':
-				return 'h'
-			print("Invalid ship direction. Please Enter either V/v or H/h")
+    # check if ship location is already in use
+    def validate_ship_locations(self, ship_locations):
+        if ship_locations:
+            if self.ship_grid:
+                for ship in self.ship_grid:
+                    for ship_location in ship_locations:
+                        if ship_location in ship:
+                            return False
+            self.ship_grid.append(ship_locations)
+            return True
+        return False
+
+    # check if user wants the ship horizontally or vertically
+    def get_ship_direction(self):
+        while True:
+            ship_direction = input("Do you want to place ship"
+                                   "[H]orizontally or "
+                                   "[V]erticlaly ?").strip().lower()
+            if ship_direction == 'v':
+                return 'v'
+            elif ship_direction == 'h':
+                return 'h'
+            print("Invalid ship direction. Please Enter either V/v or H/h")
+
+    ##################
+    # Board related functions
+    ##################
+
+    # prints the board heading
+    def print_board_heading(self):
+        print("" + " ".join([chr(c) for c in range(ord('A'), ord('A') + 10)]))
+
+    # prints the board
+    def print_board(self, ship_locations):
+        print("  ", end=' ')
+        self.print_board_heading()
+        for row in range(1, 11):
+            if row != 10:
+                print(row, end='  ')
+            else:
+                print(row, end=' ')
+            for x in 'abcdefghij':
+                if (x, row, 'v') in ship_locations:
+                    print('|', end=' ')
+                elif (x, row, 'h') in ship_locations:
+                    print('-', end=' ')
+                else:
+                    print('0', end=' ')
+            print("\n")
+
+    # gets all the locations of the ships
+    def get_ships_locations(self, player_board_ship_gird):
+        ships_locations = []
+        for ship in player_board_ship_gird:
+            for ship_location in ship:
+                ships_locations.append(ship_location)
+        return ships_locations
+
+
